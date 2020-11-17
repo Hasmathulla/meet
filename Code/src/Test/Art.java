a55b4dfc37cc51d453cfa0d0d50505746a384159
Hasmathulla <Hasmathulla@DESKTOP-3QPTKUU>
2020-11-17 18:04:30 +0530

    first commit  uploading all the files

---
+++ Code/src/Test/Art.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Art {
+	public static void main(String[ ] args)
+	{
+	}
+	
+	
+
+}
---
+++ Code/src/Test/Calsi.java
@@ -0,0 +1,20 @@
+package Test;
+
+public class Calsi {
+	
+	
+		public int add(int num1,int num2)
+
+		{
+			int r =num1 +num2;
+			return r;
+			
+		}
+		public int sub(int num1,int num2)
+		{
+			int r=num1-num2;
+			return r;
+		}
+	}
+
+
